33aac96f9a42a585521c8792a1f954062d1ef232
Miguel Antoons <miguel.antoons@gmail.com>
2026-07-20 11:16:06 +0200

    quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut res = vec![];
+        for transformation in input {
+            match transformation.1 {
+                Command::Uppercase => res.push(transformation.0.to_uppercase()),
+                Command::Trim => res.push(transformation.0.trim().to_string()),
+                Command::Append(amount) => res.push(transformation.0 + &"bar".repeat(amount)),
+            }
+        }
+        res
+    }
 }
 
 fn main() {
@@ -37,7 +47,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
